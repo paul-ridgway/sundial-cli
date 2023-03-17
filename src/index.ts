645d52651924666c9e3e3d57e8fd71d8b895e44f
Paul Ridgway <paul@ridgway.io>
2023-03-17 22:45:53 +0000

    messing with graphics!

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { SunsynkApiClient } from 'sunsynk-node-api-client';
 import { createPromptModule } from 'inquirer';
 import axiosRetry from 'axios-retry';
 import chalk from "chalk";
+import ora from 'ora';
+import { sleep } from "./utils/sleep";
+import { Icons } from "./utils/icons";
+import { FlowPayload } from "sunsynk-node-api-client/lib/types/flow";
 
 const prompt = createPromptModule();
 
@@ -40,16 +44,16 @@ async function start() {
 
   axiosRetry((client as any)._client, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
-  console.log(chalk.green("Logged in as: %s"), (await client.getUser()).data.nickname);
+  console.log(chalk.green("Logged in as: %s"), (await client.getUser()).nickname);
 
   const plants = await client.getPlants();
-  console.log("Plants:", plants.data.infos.length);
-  console.log("First plant, ID:", plants.data.infos[0].id, plants.data.infos[0].name);
+  console.log("Plants:", plants.infos.length);
+  console.log("First plant, ID:", plants.infos[0].id, plants.infos[0].name);
 
-  console.log("Plant: ", await client.getPlant(plants.data.infos[0].id));
+  console.log("Plant: ", await client.getPlant(plants.infos[0].id));
 
 
-  console.log("Generation: ", await client.getGenerationUse(plants.data.infos[0].id));
+  console.log("Generation: ", await client.getGenerationUse(plants.infos[0].id));
 
   function colorIt(str: string, good: boolean, bad: boolean) {
     if (good) {
@@ -61,41 +65,82 @@ async function start() {
     }
   }
 
-  let flow;
+  function arrows(flow: 'left' | 'right' | 'none') {
+    const v = Math.round((Date.now() / 250) % 5);
+
+    let base = '', chr = '', i = 0;
+    switch (flow) {
+      case 'left':
+        base = '◃◃◃◃◃';
+        chr = '◂';
+        i = 4 - v;
+        break;
+      case 'right':
+        base = '▹▹▹▹▹';
+        chr = '▸';
+        i = v - 1;
+        break;
+      case 'none':
+        base = '-----';
+        chr = '-';
+        i = 0;
+        break;
+    }
+
+    return (base.slice(0, i) + chr + base.slice(i + 1, 5)).substring(0, 5);
+  }
+
+  let flow: FlowPayload | null = null;
   let lastUpdate = 0;
 
   while (true) {
     if (Date.now() - lastUpdate >= 60000) {
       let [realTime, flowData] = await Promise.all([
-        client.getRealtimeData(plants.data.infos[0].id),
-        client.getFlow(plants.data.infos[0].id, new Date()),
+        client.getRealtimeData(plants.infos[0].id),
+        client.getFlow(plants.infos[0].id, new Date()),
       ]);
       flow = flowData;
-      lastUpdate = Date.parse(realTime.data.updateAt);
+      lastUpdate = Date.parse(realTime.updateAt);
     }
 
     if (flow) {
+      console.log(chalk.magentaBright('▹▸ ◃◂ ▵▴ ▿▾'));
+
+      let buffer = "";
+      
+      buffer += chalk.greenBright(flow.pvPower.toString().padStart(4));
+      buffer += ` W ${Icons.sun} `;
+      buffer += chalk.greenBright(arrows("left"));
+      buffer += chalk.whiteBright(' ┏━━━┓ ');
+      buffer += chalk.greenBright(arrows('right'));
+      buffer+= ` ${Icons.battery} ${flow.battPower} W`;
+      buffer += "\n";
+
+      buffer += chalk.whiteBright(`                ┃ I ┃          `);
+      buffer += `${flow.soc}%\n`;
+
+      buffer += flow.gridOrMeterPower.toString().padStart(4);
+      buffer += ` W ${Icons.plug} `;
+      buffer += chalk.greenBright(arrows('none'));
+      buffer += chalk.whiteBright(' ┗━━━┛ ');
+      buffer += arrows('right');
+      buffer += ` ${Icons.house} `;
+      buffer += `${flow.loadOrEpsPower} W`;
+      buffer += "\n";
+
       console.clear();
-      console.log(new Date(lastUpdate).toLocaleString());
-      console.log();
-      console.log(colorIt(`${String.fromCharCode(0xD83D, 0xDD0B)} ${flow.data.soc}% ${flow.data.battPower} W`, flow.data.toBat, flow.data.batTo));
-      console.log(colorIt(`${String.fromCharCode(0xD83C, 0xDF1E)} ${flow.data.pvPower} W`, true, false));
-      console.log(colorIt(`${String.fromCharCode(0xD83C, 0xDFE0)} ${flow.data.loadOrEpsPower} W`, false, true));
-      console.log(colorIt(`${String.fromCharCode(0xD83D, 0xDD0C)} ${flow.data.gridOrMeterPower} W`, flow.data.toGrid, flow.data.gridTo));
+      console.log(buffer);
+
     }
 
-    await sleep(1000);
+    await sleep(50);
   }
 
-  // const energy = await client.getEnergyByDay(plants.data.infos[0].id, new Date());
+  // const energy = await client.getEnergyByDay(plants.infos[0].id, new Date());
   // console.log("Energy:", JSON.stringify(energy, null, 2));
 
 
   console.log("Finished!");
-}
-
-function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 start().catch((err) => console.error('Error running demo:', err));
---
+++ src/utils/icons.ts
@@ -0,0 +1,6 @@
+export const Icons = {
+  battery: String.fromCharCode(0xD83D, 0xDD0B),
+  sun: String.fromCharCode(0xD83C, 0xDF1E),
+  house: String.fromCharCode(0xD83C, 0xDFE0),
+  plug: String.fromCharCode(0xD83D, 0xDD0C),
+}
